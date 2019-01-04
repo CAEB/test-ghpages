aa08740b8a79851ae0d8ea42e3e8a210600eafa8
CAEB <EB1912401521@163.com>
2019-01-04 16:16:52 +0800

    Updates

---
+++ precache-manifest.2b52d79ccaaba0f8dd438f947d7ea1c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "049e6743715679348eff",
+    "url": "/static/js/main.049e6743.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "049e6743715679348eff",
+    "url": "/static/css/main.17eaaaff.chunk.css"
+  },
+  {
+    "revision": "65d741f7ad1e897aff82ecb021c44fda",
+    "url": "/index.html"
+  }
+];
